a9c5317f64e0265987be553799797d51bcf2ded4
Mirza Hilmi <dev@mrz.my.id>
2025-03-01 10:44:53 +0700

    feat: read correlation id & write to response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
@@ -9,10 +11,17 @@ public class Main {
     try (
         ServerSocket serverSocket = new ServerSocket(port);
         Socket clientSocket = serverSocket.accept();
+        InputStream in = clientSocket.getInputStream();
         DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());) {
+
       serverSocket.setReuseAddress(true);
+
+      byte[] header = in.readNBytes(12);
+      byte[] messageSize = Arrays.copyOfRange(header, 8, header.length);
+
       out.writeInt(0);
-      out.writeInt(7);
+      out.write(messageSize);
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
