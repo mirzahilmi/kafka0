2049ce79026fd778bfa5142efe6e6eacd02df456
Mirza Hilmi <dev@mrz.my.id>
2025-03-03 21:35:43 +0700

    feat: implement server & client class

---
+++ src/main/java/id/my/mirzaa/kafka0/Client.java
@@ -0,0 +1,38 @@
+package id.my.mirzaa.kafka0;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+final class Client {
+    private final Socket socket;
+
+    Client() {
+        try {
+            this.socket = new Socket(Server.DEFAULT_HOST, Server.DEFAULT_PORT);
+            System.out.println("socket established");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void send() {
+        try (
+                BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()))) {
+            out.writeInt(35);
+            out.writeShort(18);
+            out.writeShort(26442);
+            out.writeInt(1333056139);
+            out.flush();
+
+            int responseSize = Server.MESSAGE_SIZE + Server.CORRELATION_ID_SIZE + Server.ERROR_CODE_SIZE;
+            byte[] raw = new byte[responseSize];
+            in.read(raw, 0, responseSize);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/id/my/mirzaa/kafka0/Server.java
@@ -0,0 +1,82 @@
+package id.my.mirzaa.kafka0;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+final class Server implements AutoCloseable, Runnable {
+    static final String DEFAULT_HOST = "localhost";
+    static final int DEFAULT_PORT = 9092;
+
+    static final int MESSAGE_SIZE = 4;
+    static final int REQUEST_API_KEY_SIZE = 2;
+    static final int REQUEST_API_VERSION_SIZE = 2;
+    static final int CORRELATION_ID_SIZE = 4;
+    static final int ERROR_CODE_SIZE = 2;
+
+    private final ServerSocket server;
+
+    Server() {
+        try {
+            this.server = new ServerSocket(DEFAULT_PORT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    Server(final ServerSocket server) {
+        this.server = server;
+    }
+
+    void listen() {
+        System.out.println("server listening...");
+        try (
+                Socket socket = server.accept();
+                BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()))) {
+
+            int requestSize = MESSAGE_SIZE + REQUEST_API_KEY_SIZE + REQUEST_API_VERSION_SIZE + CORRELATION_ID_SIZE;
+            byte[] raw = new byte[requestSize];
+            in.read(raw, 0, requestSize);
+
+            byte[] messageSize = Arrays.copyOfRange(raw, 0, MESSAGE_SIZE);
+            byte[] requestApiKey = Arrays.copyOfRange(raw, MESSAGE_SIZE,
+                    MESSAGE_SIZE +
+                            REQUEST_API_KEY_SIZE);
+            byte[] requestApiVersion = Arrays.copyOfRange(raw, MESSAGE_SIZE +
+                    REQUEST_API_KEY_SIZE,
+                    MESSAGE_SIZE + REQUEST_API_KEY_SIZE
+                            + REQUEST_API_VERSION_SIZE);
+            byte[] correlationId = Arrays.copyOfRange(raw, MESSAGE_SIZE +
+                    REQUEST_API_KEY_SIZE
+                    + REQUEST_API_KEY_SIZE,
+                    MESSAGE_SIZE + REQUEST_API_KEY_SIZE
+                            + REQUEST_API_VERSION_SIZE
+                            + CORRELATION_ID_SIZE);
+
+            int apiVersion = ByteBuffer.wrap(requestApiVersion).getShort();
+            int errorCode = apiVersion >= 0 && apiVersion <= 4 ? 0 : 35;
+
+            out.write(messageSize);
+            out.write(correlationId);
+            out.writeShort(errorCode);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        listen();
+    }
+
+    @Override
+    public void close() throws Exception {
+        server.close();
+    }
+}
